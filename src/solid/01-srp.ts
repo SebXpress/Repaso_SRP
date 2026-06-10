28619c9a50c1b3f6bbcc84315a06d07075d829e2
SebXpress <benalcazarsebas8173@proton.me>
2026-06-10 08:07:30 -0500

    refactor: centralizar peticiones de datos en nueva clase UserService

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -5,34 +5,18 @@ interface User {
 
 // Esta clase viola el Principio de Responsabilidad Única (SRP)
 class UserBloc {
-
-    loadUser( id: number ) {
-        // Simula la carga de un usuario
-        console.log('Cargando usuario con id:', id);
-    }
-
-    saveUser( user: User ) {
-        // Simula el guardado en base de datos
-        console.log('Guardando en base de datos:', user );
-    }
-
     notifyUser() {
         // Simula el envío de notificaciones
         console.log('Enviando correo a los usuarios');
     }
-
-    
-
 }
 
 const userBloc = new UserBloc();
-
-userBloc.loadUser(10);
-userBloc.saveUser({ id: 10, name: 'Fernando' });
 userBloc.notifyUser();
 
 
-
+// Paso 1: Identificar que onAddSubscription no tiene relación directa con UserBloc.
+// Extraer este método a una nueva clase independiente llamada SubscriptionBloc e instanciarla.
 class SubscriptionBloc{
     onAddSubscription( subscriptionId: number ) {
         // Simula la gestión de suscripciones
@@ -42,3 +26,21 @@ class SubscriptionBloc{
 
 const subscriptionBloc = new SubscriptionBloc();
 subscriptionBloc.onAddSubscription(1234);
+
+// Paso 2: Identificar que cargar y guardar interactúan con la base de datos/API. 
+// Extraer loadUser y saveUser a una nueva clase UserService.
+class UserService{
+    loadUser( id: number ) {
+        // Simula la carga de un usuario
+        console.log('Cargando usuario con id:', id);
+    }
+
+    saveUser( user: User ) {
+        // Simula el guardado en base de datos
+        console.log('Guardando en base de datos:', user );
+    }
+}
+
+const useService = new UserService ();
+useService.loadUser(10);
+useService.saveUser({ id: 10, name: 'Fernando' });
